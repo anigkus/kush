ce7d84e9936e74cc2e142f056ff5b7b57e1dae7b
anigkus <anigkus@gmail.com>
2023-01-12 15:02:50 +0800

    feat:(sh) add install.sh and update build.sh file

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 # Copyright 2023 The https://github.com/anigkus Authors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
+# you may not use this V_FILE except in compliance with the License.
 # You may obtain a copy of the License at#
 
 #     http://www.apache.org/licenses/LICENSE-2.0#
@@ -18,49 +18,54 @@
 #  
 
 #go args
-goos=$1
-goarch=$2
+V_GOOS=$1
+V_GOARCH=$2
 
 # build args
-arch="64"
-file='kush'
+V_ARCH="amd64"
+V_FILE='kush'
 
 if [ ! -x "$(command -v go)" ] ||  [ ! -x "$(command -v upx)" ] ||  [ ! -x "$(command -v tar)" ]; then
   echo 'Error: [ go | upx | tar ] is not installed.' >&2
   exit 1
 fi
 
-if [ ! -z "$goos" ] && [ ! -z "$goarch" ]; then
-    if [ "$goos" == "windows" ]; then
-        file='kush.exe'
+if [ ! -z "$V_GOOS" ] || [ ! -z "$V_GOARCH" ]; then
+    if [ "$V_GOOS" == "windows" ]; then
+        V_FILE='kush.exe'
     fi
-    if [[ "$goarch" =~ ^[32|386]$ ]]; then
-        arch='32'
+    if [[ "$V_GOARCH" =~ ^[32|386]$ ]]; then
+        V_ARCH='32'
     fi
-    CGO_ENABLED=0 GOOS=$goos GOARCH=$goarch go build -ldflags="-s -w" -o build/$goos/$goarch/$file  && upx --best --lzma build/$goos/$goarch/$file && cd build/$goos/$goarch && tar -czf kush_$goos_x$arch.tar.gz $file && rm -f $file && cd -
+    CGO_ENABLED=0 GOOS=$V_GOOS GOARCH=$V_GOARCH go build -ldflags="-s -w" -o build/$V_GOOS/$V_GOARCH/$V_FILE  && upx --best --lzma build/$V_GOOS/$V_GOARCH/$V_FILE && cd build/$V_GOOS/$V_GOARCH && tar -czf kush_$V_GOOS_x$V_ARCH.tar.gz $V_FILE && rm -f $V_FILE && cd -
 else
     OS="`uname`"
     case $OS in
     'Linux')
-        CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags="-s -w" -o build/linux/amd64/kush && upx --best --lzma build/linux/amd64/kush && cd build/linux/amd64 && tar -czf kush_linux_x64.tar.gz kush && rm -f kush && cd -
+        CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags="-s -w" -o build/linux/amd64/kush && upx --best --lzma build/linux/amd64/kush && cd build/linux/amd64 && tar -czf kush_linux_amd64.tar.gz kush && rm -f kush && cd -
         ;;
     'FreeBSD')
         echo "Unsupported OS"
+        exit 1
         ;;
     'WindowsNT')
         echo "Unsupported OS"
+        exit 1
         ;;
     'Darwin') 
-        CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -ldflags="-s -w" -o build/darwin/amd64/kush && upx --best --lzma build/darwin/amd64/kush && cd build/darwin/amd64 && tar -czf kush_darwin_x64.tar.gz kush && rm -f kush && cd -
+        CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -ldflags="-s -w" -o build/darwin/amd64/kush && upx --best --lzma build/darwin/amd64/kush && cd build/darwin/amd64 && tar -czf kush_darwin_amd64.tar.gz kush && rm -f kush && cd -
         ;;
     'SunOS')
-      echo "Unsupported OS"
+        echo "Unsupported OS"
+        exit 1
         ;;
     'AIX') 
         echo "Unsupported OS"
+        exit 1
         ;;
     *) 
         echo "Unsupported OS"
+        exit 1
         ;;
     esac
 fi
---
+++ install.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/env bash
+# Copyright 2023 The https://github.com/anigkus Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at#
+
+#     http://www.apache.org/licenses/LICENSE-2.0#
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#  
+#  bash install script
+#  
+
+V_VERSION=$1
+V_GOARCH=$2
+
+
+if [ -z "$V_VERSION" ]; then
+    V_VERSION=`sudo curl https://github.com/anigkus/kush/releases | grep -oiE "<a([^>]+) class=\"Link--primary\">([^<]+)</a>" | awk -F '>' '{print $2}' | awk -F '<' '{print $1}'`
+fi
+
+OS="`uname`"
+case $OS in
+'Linux')
+    V_FILE='kush_linux_amd64.tar.gz'
+    ;;
+'FreeBSD')
+    echo "Unsupported OS"
+    exit 1
+    ;;
+'WindowsNT')
+    echo "Unsupported OS"
+    exit 1
+    ;;
+'Darwin') 
+    V_FILE='kush_darwin_amd64.tar.gz'
+    ;;
+'SunOS')
+    echo "Unsupported OS"
+    exit 1
+    ;;
+'AIX') 
+    echo "Unsupported OS"
+    exit 1
+    ;;
+*) 
+    echo "Unsupported OS"
+    exit 1
+    ;;
+esac
+
+sudo curl -sLX GET https://github.com/anigkus/kush/releases/download/v$V_VERSION/$V_FILE | sudo tar -xz -C /usr/local/bin/ 2>/dev/null
