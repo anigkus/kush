dc7fe4cf986aed85b73e6736401c56c923afc8b5
anigkus <anigkus@gmail.com>
2023-09-20 18:04:16 +0800

    feat:(brew) update kush.rb sha256

--- kush.rb
+++ kush.rb
@@ -2,7 +2,7 @@ class Kush < Formula
   desc "Cross-platform command-line SSH remote address connection tool"
   homepage "https://github.com/anigkus/kush"
   url "https://github.com/anigkus/kush/archive/refs/tags/v0.0.1.tar.gz"
-  sha256 "e6f89db386f3e55ac450e3a350dfcdfa5fdb254258428fe0a95ff3d219bd3cba"
+  sha256 "7bba03df7e6a1d8aeeb60942d49dd469ed0f06f1561c8589fa1647fdb671ecb9"
   license "Apache-2.0"
   head "https://github.com/anigkus/kush.git", branch: "main"
   depends_on "go" => :build
